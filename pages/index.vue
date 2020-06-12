93d8f2bee84daa9df829fb57798ce2244508e831
Jack Brittan <jackbrittan@gmail.com>
2020-06-12 06:36:37 +0100

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        live-stream
+      </h1>
+      <video-player  class="video-player-box"
+                 ref="videoPlayer"
+                 :options="playerOptions"
+      >
+  </video-player>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'video.js/dist/video-js.css'
+import { videoPlayer } from 'vue-video-player'
+
+export default {
+  components: {
+    videoPlayer
+  },
+  data() {
+      return {
+        playerOptions: {
+          // videojs options
+          muted: true,
+          language: 'en',
+          playbackRates: [0.7, 1.0, 1.5, 2.0],
+          sources: [{
+            type: "video/mp4",
+            src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+          }]
+        }
+      }
+    },
+    mounted() {
+      console.log('this is current player instance object', this.player)
+    },
+    computed: {
+      player() {
+        return this.$refs.videoPlayer.player
+      }
+    },
+    methods: {
+      // listen event
+      onPlayerPlay(player) {
+        // console.log('player play!', player)
+      },
+      onPlayerPause(player) {
+        // console.log('player pause!', player)
+      },
+      // ...player event
+
+      // or listen state event
+      playerStateChanged(playerCurrentState) {
+        // console.log('player current update state', playerCurrentState)
+      },
+
+      // player is ready
+      playerReadied(player) {
+        console.log('the player is readied', player)
+        // you can use it to do something...
+        // player.[methods]
+      }
+    }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
